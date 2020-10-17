e110f79795773bac19f606ee741c06c022d5d98f
andysteel <andersoninfonet@gmail.com>
2020-10-17 01:53:13 -0300

    Update app.js
    change to port 8080

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 // database connection
 const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@devcluster-1vl5x.mongodb.net/node-auth?retryWrites=true&w=majority`;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then(async (result) => await app.listen(3000))
+  .then(async (result) => await app.listen(8080))
   .catch(async (err) => await appLogger.log('error',err));
 
 //log
